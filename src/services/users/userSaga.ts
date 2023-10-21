ceb1ede284cabbd5a070c645ceaa8a78cc10d23b
Drizio <driziob@gmail.com>
2023-10-22 00:19:58 +0200

    Removed useless console.log

--- src/services/users/userSaga.ts
+++ src/services/users/userSaga.ts
@@ -16,7 +16,6 @@ function* getUserSaga({payload: id}: PayloadAction<string>) {
     const response: Response = yield fetch(`https://reqres.in/api/users/${id}`);
 
     const json: ApiResponseType = yield response.json();
-    console.log(json, 3333);
     if (json.data) {
       yield put(getUserSuccess(json.data));
     } else {
