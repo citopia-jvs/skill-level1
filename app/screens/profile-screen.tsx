d51c6ee05b112eefbd6560aadd49438ccceb9f66
Draktors <hespelenzo@gmail.com>
2023-11-08 12:50:49 +0100

    feat: Add React Hook Form for inputs validation

--- app/screens/profile-screen.tsx
+++ app/screens/profile-screen.tsx
@@ -1,43 +1,84 @@
-import React, { useState } from "react";
+import React from "react";
 import { View, TextInput, StyleSheet, Text } from "react-native";
+import { useForm, Controller } from "react-hook-form";
 
 import { UPDATE_USER_INFO } from "~store/actions";
 import { useAppSelector } from "~/hooks/useAppSelector";
 import { useAppDispatch } from "~/hooks/useAppDispatch";
+import { UserInfo } from "~services/user/types";
 
 export const ProfileScreen = () => {
   const dispatch = useAppDispatch();
   const userInfo = useAppSelector((state) => state.user);
 
-  const [userForm, setUserForm] = useState({
-    firstName: userInfo.firstName,
-    lastName: userInfo.lastName,
+  const {
+    control,
+    handleSubmit,
+    formState: { isValid, errors },
+  } = useForm({
+    defaultValues: {
+      firstName: userInfo.firstName,
+      lastName: userInfo.lastName,
+    },
+    mode: "onChange",
   });
 
-  const handleInputChange = (field: keyof typeof userForm, value: string) => {
-    setUserForm({ ...userForm, [field]: value });
+  const validation = {
+    firstName: {
+      required: "Ce champ est obligatoire",
+    },
+    lastName: {
+      required: "Ce champ est obligatoire",
+    },
   };
 
-  const handleSubmit = () => {
-    dispatch({ type: UPDATE_USER_INFO, payload: { ...userForm, id: 2 } });
+  const updateUser = (userForm: UserInfo) => {
+    if (isValid) {
+      dispatch({ type: UPDATE_USER_INFO, payload: { ...userForm, id: 2 } });
+    }
   };
 
   return (
     <View>
-      <Text>First Name</Text>
-      <TextInput
-        value={userForm.firstName}
-        onChangeText={(value) => handleInputChange("firstName", value)}
-        onBlur={handleSubmit}
-        style={styles.input}
+      <Controller
+        control={control}
+        name="firstName"
+        rules={validation["firstName"]}
+        render={({ field: { onChange, value } }) => (
+          <View>
+            <Text>First Name</Text>
+
+            <TextInput
+              placeholder="First Name"
+              value={value}
+              onChangeText={onChange}
+              onBlur={handleSubmit(updateUser)}
+              style={styles.input}
+            />
+          </View>
+        )}
       />
-      <Text>Last Name</Text>
-      <TextInput
-        value={userForm.lastName}
-        onChangeText={(value) => handleInputChange("lastName", value)}
-        onBlur={handleSubmit}
-        style={styles.input}
+      {errors.firstName && <Text>{errors.firstName.message}</Text>}
+
+      <Controller
+        control={control}
+        name="lastName"
+        rules={validation["lastName"]}
+        render={({ field: { onChange, value } }) => (
+          <View>
+            <Text>Last Name</Text>
+
+            <TextInput
+              placeholder="Last Name"
+              value={value}
+              onChangeText={onChange}
+              onBlur={handleSubmit(updateUser)}
+              style={styles.input}
+            />
+          </View>
+        )}
       />
+      {errors.lastName && <Text>{errors.lastName.message}</Text>}
     </View>
   );
 };
